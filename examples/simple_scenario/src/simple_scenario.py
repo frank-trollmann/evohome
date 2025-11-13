ca3413f730c51e1abd2ee134d281002482132e17
frank-trollmann <frank.trollmann@code.berlin>
2025-11-13 09:14:18 +0100

    feat: updated simple scenario with leisure activities

--- examples/simple_scenario/src/simple_scenario.py
+++ examples/simple_scenario/src/simple_scenario.py
@@ -1,6 +1,7 @@
 from domain_model.changes.move_in_change import Move_In_Change
 from domain_model.changes.obligation_add_change import Obligation_Add_Change
 from domain_model.changes.obligation_remove_change import Obligation_Remove_Change
+from domain_model.leisure_activity import Leisure_Activity
 from domain_model.obligation import Obligation
 from domain_model.scenario import Scenario_Configuration
 from domain_model.changes.move_out_change import Move_Out_Change
@@ -26,7 +27,7 @@ def create_simple_scenario():
     bedroom1 = Room("Bedroom 1", 663, 490, [Room.FUNCTION_SLEEP])
     bedroom2 = Room("Bedroom 2", 170, 230, [Room.FUNCTION_SLEEP])
     bedroom3 = Room("Bedroom 3", 170, 500, [Room.FUNCTION_SLEEP])
-    storage = Room("Storage", 100, 370, [Room.FUNCTION_OFFICE, Room.FUNCTION_LEISURE])
+    office = Room("Storage", 100, 370, [Room.FUNCTION_OFFICE, Room.FUNCTION_LEISURE])
     bathroom_1 = Room("Bath 1", 670, 320, [Room.FUNCTION_BATHROOM])
     bathroom_2 = Room("Bath 2", 260, 260, [Room.FUNCTION_BATHROOM])
     kitchen = Room("Kitchen", 510, 180, [Room.FUNCTION_COOK])
@@ -38,7 +39,7 @@ def create_simple_scenario():
     house.add_room(bedroom1)
     house.add_room(bedroom2)
     house.add_room(bedroom3)
-    house.add_room(storage)
+    house.add_room(office)
     house.add_room(bathroom_1)
     house.add_room(bathroom_2)
     house.add_room(kitchen)
@@ -48,7 +49,7 @@ def create_simple_scenario():
 
     house.add_transtion(bedroom2, hallway_1)
     house.add_transtion(bedroom3, hallway_1)
-    house.add_transtion(storage, hallway_1)
+    house.add_transtion(office, hallway_1)
     house.add_transtion(bathroom_2, hallway_1)
     house.add_transtion(hallway_1, hallway_2) 
     house.add_transtion(hallway_2, kitchen)
@@ -57,32 +58,12 @@ def create_simple_scenario():
     house.add_transtion(hallway_2, bedroom1)
     house.add_transtion(bedroom1, bathroom_1)
 
-    # create persons
-    parent1 = Person("Arny", (255, 255, 0),wake_up_time= time(6,00), sleep_time = time(22,00))
-    parent1.sleep_room = bedroom1
-    parent1.move_to_room(bedroom1)
-    scenario.persons.append(parent1)
 
-    parent2 = Person("Betty", (0, 255, 255),wake_up_time = time(5,00), sleep_time = time(21,00))
-    parent2.sleep_room = bedroom1
-    parent2.move_to_room(bedroom1)
-    scenario.persons.append(parent2)
-
-    child1 = Person("Coline", (255, 0, 255),wake_up_time = time(6,00), sleep_time = time(20,00))
-    child1.sleep_room = bedroom2
-    child1.move_to_room(bedroom2)
-    scenario.persons.append(child1)
-
-    child2 = Person("Dave", (255, 0, 0),wake_up_time = time(6,00), sleep_time = time(21,00))
-    child2.sleep_room = bedroom3
-    child2.move_to_room(bedroom3)
-    scenario.persons.append(child2)
-
-    # add Obligations
+    # create Obligations
     work_parent_1_obligation = Obligation("Work", 
                                 start_time= time(7,00), 
                                 end_time = time(14,00), 
-                                location = storage, 
+                                location = office, 
                                 weekdays = [0,1,2,3,4,5])
     work_parent_2_obligation = Obligation("Work", 
                                 start_time= time(6,00), 
@@ -115,36 +96,81 @@ def create_simple_scenario():
                                 location = None, 
                                 weekdays = [0,1,2,3,4,5])
     
-    parent1.add_obligation(work_parent_1_obligation)
-    parent1.add_obligation(get_kids_obligation)
-    parent1.add_obligation(cook_obligation)
-    parent1.add_obligation(dinner_obligation)
+    # create leisure_activities
+    alone_time_parent_1 = Leisure_Activity("Alone Time", bedroom1)
+    play_pc = Leisure_Activity("Play Games",office)
+    watch_tv = Leisure_Activity("TV",living_room)
 
-    parent2.add_obligation(work_parent_2_obligation)
-    parent2.add_obligation(dinner_obligation)
-    parent2.add_obligation(bring_to_bed_obligation)
+    alone_time_parent_2 = Leisure_Activity("Alone Time", bedroom1)
+    porch_reading = Leisure_Activity("Porch reading", porch)
+    inside_reading = Leisure_Activity("Inside reading", living_room)
 
-    child1.add_obligation(school_obligation)
-    child1.add_obligation(dinner_obligation)
-    child1.add_obligation(bring_to_bed_obligation)
+    alone_time_child_1 = Leisure_Activity("Alone Time", bedroom2)
+    shopping = Leisure_Activity("Shopping", weekdays = [0,1,2,3,4,5])
 
-    child2.add_obligation(school_obligation)
-    child2.add_obligation(dinner_obligation)
+    alone_time_child_2 = Leisure_Activity("Alone Time", bedroom3)
 
+
+    
+     # create persons
+    parent_1 = Person("Arny", (255, 255, 0),wake_up_time= time(6,00), sleep_time = time(22,00))
+    parent_1.sleep_room = bedroom1
+    parent_1.move_to_room(bedroom1)
+    scenario.persons.append(parent_1)
+    parent_1.add_obligation(work_parent_1_obligation)
+    parent_1.add_obligation(get_kids_obligation)
+    parent_1.add_obligation(cook_obligation)
+    parent_1.add_obligation(dinner_obligation)
+    parent_1.add_leisure_activity(alone_time_parent_1, 1)
+    parent_1.add_leisure_activity(play_pc, 2)
+    parent_1.add_leisure_activity(watch_tv, 2)
+
+    parent_2 = Person("Betty", (0, 255, 255),wake_up_time = time(5,00), sleep_time = time(21,00))
+    parent_2.sleep_room = bedroom1
+    parent_2.move_to_room(bedroom1)
+    scenario.persons.append(parent_2)
+    parent_2.add_obligation(work_parent_2_obligation)
+    parent_2.add_obligation(dinner_obligation)
+    parent_2.add_obligation(bring_to_bed_obligation)
+    parent_2.add_leisure_activity(alone_time_parent_2,1)
+    parent_2.add_leisure_activity(porch_reading,4)
+    parent_2.add_leisure_activity(inside_reading,2)
+
+
+    child_1 = Person("Coline", (255, 0, 255),wake_up_time = time(6,00), sleep_time = time(20,00))
+    child_1.sleep_room = bedroom2
+    child_1.move_to_room(bedroom2)
+    scenario.persons.append(child_1)
+    child_1.add_obligation(school_obligation)
+    child_1.add_obligation(dinner_obligation)
+    child_1.add_obligation(bring_to_bed_obligation)
+    child_1.add_leisure_activity(alone_time_child_1,4)
+    child_1.add_leisure_activity(shopping,2)
+    child_1.add_leisure_activity(watch_tv,2)
+
+    child_2 = Person("Dave", (255, 0, 0),wake_up_time = time(6,00), sleep_time = time(21,00))
+    child_2.sleep_room = bedroom3
+    child_2.move_to_room(bedroom3)
+    scenario.persons.append(child_2)
+    child_2.add_obligation(school_obligation)
+    child_2.add_obligation(dinner_obligation)
+    child_2.add_leisure_activity(alone_time_child_1,2)
+    child_2.add_leisure_activity(watch_tv,4)
+    child_2.add_leisure_activity(play_pc,4)
 
     # create changes
-    child_2_move_out = Move_Out_Change(datetime(year = 2021, month = 6, day = 2, hour = 0, minute = 0), child2)
-    child_2_move_in = Move_In_Change(datetime(year = 2021, month = 9, day = 2, hour = 0, minute = 0), child2)
+    child_2_move_out = Move_Out_Change(datetime(year = 2021, month = 6, day = 2, hour = 0, minute = 0), child_2)
+    child_2_move_in = Move_In_Change(datetime(year = 2021, month = 9, day = 2, hour = 0, minute = 0), child_2)
     scenario.changes.append(child_2_move_out)
     scenario.changes.append(child_2_move_in)
 
-    remove_cooking_change = Obligation_Remove_Change(datetime(year = 2021, month = 3, day = 2, hour = 0, minute = 0), parent1, "Cook")
+    remove_cooking_change = Obligation_Remove_Change(datetime(year = 2021, month = 3, day = 2, hour = 0, minute = 0), parent_1, "Cook")
     cook2_obligation = Obligation("Cook2", 
                                 start_time= time(15,30), 
                                 end_time = time(16,30), 
                                 location = kitchen, 
                                 weekdays = [0,1,2,3,4,5])
-    add_cooking_change = Obligation_Add_Change(datetime(year = 2021, month = 3, day = 2, hour = 0, minute = 0), parent1, cook2_obligation)
+    add_cooking_change = Obligation_Add_Change(datetime(year = 2021, month = 3, day = 2, hour = 0, minute = 0), parent_1, cook2_obligation)
     scenario.changes.append(remove_cooking_change)
     scenario.changes.append(add_cooking_change) 
 
