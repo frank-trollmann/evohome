9a4127ec9743c562a2b276ff2457a09a927ce4b8
frank-trollmann <frank.trollmann@code.berlin>
2025-11-13 07:01:27 +0100

    chore: cleaned up main tick loop of person simulation

--- simulation/person_simulator.py
+++ simulation/person_simulator.py
@@ -11,74 +11,85 @@ from simulation.pathfinding import Pathfinding
 
 class Person_Simulator:
 
-    MODE_ACTIVITY = 0
-    MODE_MOVEMENT = 1
+    MODE_UNDECIDED = 0
+    MODE_OBLIGATION = 1
+    MODE_LEISURE = 2
 
     def __init__(self, simulation, person):
         self.simulation = simulation
         self.person = person
         self.schedule = []
         self.last_simulated_day = -1
-        self.current_schedule_item = None
-        self.mode = Person_Simulator.MODE_ACTIVITY
+        self.current_task = None
+        self.moving = False
+        self.mode = Person_Simulator.MODE_LEISURE
         self.path = None
 
+
     def tick(self):
+        now = self.simulation.current_time.time()
         if self.simulation.current_time.day != self.last_simulated_day:
             self.make_day_schedule()
-
-        now = self.simulation.current_time.time()
 
         while len(self.schedule) > 0 and now > self.schedule[0].end_time:
             del self.schedule[0]
 
-        if self.mode == Person_Simulator.MODE_MOVEMENT:
-            self.move()
-        elif self.mode == Person_Simulator.MODE_ACTIVITY:
+        if self.moving:
+            self.move_tick()
+            return
+        
+        if self.mode == Person_Simulator.MODE_OBLIGATION:
+            if now > self.current_task.end_time:
+                self.current_task = None
+                self.mode = Person_Simulator.MODE_UNDECIDED
+   
+        if self.mode == Person_Simulator.MODE_UNDECIDED or self.mode == Person_Simulator.MODE_LEISURE:
+            if now >= self.schedule[0].start_time:
+                print("picking obligation " + self.schedule[0].description)
+                self.mode = Person_Simulator.MODE_OBLIGATION
+                self.current_task = self.schedule[0]
+                self.start_move(self.schedule[0].get_room())
 
-            # go to schedule position, if not there
-            if now > self.schedule[0].start_time:
-                if(self.current_schedule_item != self.schedule[0]):
-                    self.current_schedule_item = self.schedule[0]
-                    self.start_movement(self.schedule[0].get_room())
-                    #self.person.move_to_room(self.schedule[0].get_room())
-                return
+        if self.mode == Person_Simulator.MODE_UNDECIDED:
+            self.mode = Person_Simulator.MODE_LEISURE
+            self.pick_leisure_activity()
+            return
+        
 
-            # pick leisure activity if not picked already
-            if(self.current_schedule_item != None):
-                self.current_schedule_item = None
-                leisure_rooms = [self.person.sleep_room, None]
-                leisure_rooms.extend(self.simulation.house.get_rooms_by_function(Room.FUNCTION_LEISURE))
-                picked_room = random.choice(leisure_rooms)
-                self.start_movement(picked_room)
-                #self.person.move_to_room(picked_room)
 
-    def move(self):
+    def pick_leisure_activity(self):
+        leisure_rooms = [self.person.sleep_room, None]
+        leisure_rooms.extend(self.simulation.house.get_rooms_by_function(Room.FUNCTION_LEISURE))
+        picked_room = random.choice(leisure_rooms)
+        print("picking leisure time " + str(picked_room))
+        self.start_move(picked_room)
+
+    def move_tick(self):
         """
             Moves the person towards the room.
             this makes a step towards the next room in self.path.
         """
         if len(self.path) == 0:
-            self.mode = Person_Simulator.MODE_ACTIVITY
+            self.moving = False
             return
         
         next_room = self.path.popleft()
         self.person.move_to_room(next_room)
 
 
-    def start_movement(self, target_room):
+    def start_move(self, target_room):
         """
             Starts moving the person towards the room.
             This calculates a path and sets the simulator to MODE_MOVEMENT
         """
         if target_room is None or self.person.room is None:
+            self.moving = False
             self.person.move_to_room(target_room)
-            self.mode = Person_Simulator.MODE_ACTIVITY
             return
         
+        self.moving = True
         pathfinding = Pathfinding.instance()
         self.path = deque(pathfinding.get_path(self.simulation.house, self.person.room, target_room))
-        self.mode = Person_Simulator.MODE_MOVEMENT
 
         
 
