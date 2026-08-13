3e62e2dc5dd03688b13a73501b1e9ae1d863d1db
frank-trollmann <frank.trollmann@code.berlin>
2026-08-13 10:57:58 +0200

    fix: minor fixes in function comments and descriptions

--- domain_model/changes/obligation_remove_change.py
+++ domain_model/changes/obligation_remove_change.py
@@ -13,7 +13,7 @@ class Obligation_Remove_Change(Scheduled_Change):
             Args:
                 datetime (datetime): the date and time when the change should be executed.
                 person (Person): the person who moves in.
-                obligation (Obligation): the obligation to add.
+                obligation (Obligation): the obligation to remove.
         """
         super().__init__(datetime)
         self.person = person
